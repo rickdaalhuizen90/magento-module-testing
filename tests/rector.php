38ea4654643154924e06cd43f1e28dc26d37aaa3
Rick Daalhuizen <rick.daalhuizen@outlook.com>
2024-07-29 21:59:07 +0200

    feat: add rector configuration

--- tests/rector.php
+++ tests/rector.php
@@ -4,19 +4,103 @@ declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
-use Rector\TypeDeclaration\Rector\Property\TypedPropertyFromStrictConstructorRector;
-use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
-use Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector;
+use Rector\CodeQuality\Rector\Class_\CompleteDynamicPropertiesRector;
+use Rector\CodeQuality\Rector\ClassMethod\OptionalParametersAfterRequiredRector;
+use Rector\TypeDeclaration\Rector\StmtsAwareInterface\DeclareStrictTypesRector;
+use Rector\DeadCode\Rector\If_\RemoveAlwaysTrueIfConditionRector;
+use Rector\DeadCode\Rector\For_\RemoveDeadContinueRector;
+use Rector\DeadCode\Rector\Plus\RemoveDeadZeroAndOneOperationRector;
+use Rector\DeadCode\Rector\Assign\RemoveDoubleAssignRector;
+use Rector\DeadCode\Rector\Array_\RemoveDuplicatedArrayKeyRector;
+use Rector\DeadCode\Rector\Switch_\RemoveDuplicatedCaseInSwitchRector;
+use Rector\DeadCode\Rector\StmtsAwareInterface\RemoveJustPropertyFetchForAssignRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedConstructorParamRector;
+use Rector\DeadCode\Rector\Foreach_\RemoveUnusedForeachKeyRector;
+use Rector\DeadCode\Rector\ClassConst\RemoveUnusedPrivateClassConstantRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
+use Rector\DeadCode\Rector\Property\RemoveUnusedPrivatePropertyRector;
+use Rector\DeadCode\Rector\Assign\RemoveUnusedVariableAssignRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessParamTagRector;
+use Rector\DeadCode\Rector\ClassMethod\RemoveUselessReturnTagRector;
+use Rector\DeadCode\Rector\Property\RemoveUselessVarTagRector;
+use Rector\DeadCode\Rector\If_\SimplifyIfElseWithSameContentRector;
+use Rector\EarlyReturn\Rector\If_\ChangeAndIfToEarlyReturnRector;
+use Rector\EarlyReturn\Rector\If_\ChangeIfElseValueAssignToEarlyReturnRector;
+use Rector\EarlyReturn\Rector\Foreach_\ChangeNestedForeachIfsToEarlyContinueRector;
+use Rector\EarlyReturn\Rector\If_\ChangeNestedIfsToEarlyReturnRector;
+use Rector\EarlyReturn\Rector\Return_\PreparedValueToEarlyReturnRector;
+use Rector\EarlyReturn\Rector\If_\RemoveAlwaysElseRector;
+use Rector\EarlyReturn\Rector\Return_\ReturnBinaryOrToEarlyReturnRector;
+use Rector\EarlyReturn\Rector\StmtsAwareInterface\ReturnEarlyIfVariableRector;
+use Rector\Instanceof_\Rector\Ternary\FlipNegatedTernaryInstanceofRector;
+use Rector\Naming\Rector\Foreach_\RenameForeachValueVariableToMatchExprVariableRector;
+use Rector\Naming\Rector\Foreach_\RenameForeachValueVariableToMatchMethodCallReturnTypeRector;
+use Rector\Naming\Rector\Class_\RenamePropertyToMatchTypeRector;
+use Rector\Php74\Rector\LNumber\AddLiteralSeparatorToNumberRector;
+use Rector\Php74\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
+use Rector\Php74\Rector\Closure\ClosureToArrowFunctionRector;
+use Rector\Php74\Rector\Assign\NullCoalescingOperatorRector;
+use Rector\Php74\Rector\Ternary\ParenthesizeNestedTernaryRector;
+use Rector\Strict\Rector\BooleanNot\BooleanInBooleanNotRuleFixerRector;
+use Rector\Strict\Rector\If_\BooleanInIfConditionRuleFixerRector;
+use Rector\Strict\Rector\Ternary\BooleanInTernaryOperatorRuleFixerRector;
+use Rector\Strict\Rector\Ternary\DisallowedShortTernaryRuleFixerRector;
+use Rector\Visibility\Rector\ClassMethod\ExplicitPublicClassMethodRector;
+use Rector\PHPUnit\Set\PHPUnitSetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rule(TypedPropertyFromStrictConstructorRector::class);
-    $rectorConfig->rule(SimplifyIfReturnBoolRector::class);
-    $rectorConfig->rule(NewlineBeforeNewAssignSetRector::class);
+    $rectorConfig->paths([
+        __DIR__ . '/app',
+    ]);
+
+    $rectorConfig->rules([
+        CompleteDynamicPropertiesRector::class,
+        OptionalParametersAfterRequiredRector::class,
+        DeclareStrictTypesRector::class,
+        RemoveAlwaysTrueIfConditionRector::class,
+        RemoveDeadContinueRector::class,
+        RemoveDeadZeroAndOneOperationRector::class,
+        RemoveDoubleAssignRector::class,
+        RemoveDuplicatedArrayKeyRector::class,
+        RemoveDuplicatedCaseInSwitchRector::class,
+        RemoveJustPropertyFetchForAssignRector::class,
+        RemoveUnusedConstructorParamRector::class,
+        RemoveUnusedForeachKeyRector::class,
+        RemoveUnusedPrivateClassConstantRector::class,
+        RemoveUnusedPrivateMethodParameterRector::class,
+        RemoveUnusedPrivateMethodRector::class,
+        RemoveUnusedPrivatePropertyRector::class,
+        RemoveUnusedVariableAssignRector::class,
+        RemoveUselessParamTagRector::class,
+        RemoveUselessReturnTagRector::class,
+        RemoveUselessVarTagRector::class,
+        SimplifyIfElseWithSameContentRector::class,
+        ChangeAndIfToEarlyReturnRector::class,
+        ChangeIfElseValueAssignToEarlyReturnRector::class,
+        ChangeNestedForeachIfsToEarlyContinueRector::class,
+        ChangeNestedIfsToEarlyReturnRector::class,
+        PreparedValueToEarlyReturnRector::class,
+        RemoveAlwaysElseRector::class,
+        ReturnBinaryOrToEarlyReturnRector::class,
+        ReturnEarlyIfVariableRector::class,
+        FlipNegatedTernaryInstanceofRector::class,
+        RenameForeachValueVariableToMatchExprVariableRector::class,
+        RenameForeachValueVariableToMatchMethodCallReturnTypeRector::class,
+        RenamePropertyToMatchTypeRector::class,
+        AddLiteralSeparatorToNumberRector::class,
+        ArraySpreadInsteadOfArrayMergeRector::class,
+        ClosureToArrowFunctionRector::class,
+        ParenthesizeNestedTernaryRector::class,
+        BooleanInBooleanNotRuleFixerRector::class,
+        BooleanInIfConditionRuleFixerRector::class,
+    ]);
 
     $rectorConfig->sets([
         SetList::CODE_QUALITY,
         SetList::CODING_STYLE,
         SetList::NAMING,
         SetList::TYPE_DECLARATION,
+        SetList::PHP_74,
     ]);
 };
